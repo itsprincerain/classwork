8eacb81ea8df347f9769a48da070d6c5e54ac1ff
Rainier <rainier.1123@gmail.com>
2022-08-11 16:03:32 -0400

    loop

---
+++ loop/app.js
@@ -0,0 +1,64 @@
+//inside paranthesis we have
+//three statements:
+
+//state 1:
+//declare and initialize a variable
+//scopeds to the for loop
+
+//statemet 2:
+// a condition that when true runs the
+//code in the for loop
+
+
+//state 3:
+//incrementing of the variable in
+//order ti eventually break the loop
+
+//for (let i = 0; i < 10; i++) {
+//  console.log(i);
+// if (i == 5)
+
+//}
+
+//1: declare and initialize variable
+//2: check if condition is true
+//3: console log runs and prints 0
+//4: the variable i increments from 0 to 1
+
+//5: check if condition is true
+//6: console log runs and prints 1
+//7: i increments from 1 to 2
+//8: check if condition is true (it's not)
+//9: exit loop, continue with rest of code
+
+//break
+//continue
+
+//exercise 1:
+//create for loop counting down 10 to 1 
+// for (let i = 10; i > 0; i--) {
+//  console.log(i)
+// }
+
+//exercise 2:
+// write a for loop for the given output
+// odd numbers
+
+//for (let i = 1; index <10; i+=2) {
+//  console.log(i)
+//}
+
+//exercise 3:
+//create a loop that outputs multiples of 3 starting at 6 ending at 60
+//for (let i = 6; index <63; i+=3) {
+// console.log(i)
+//}
+
+//exercise 4:
+//for (let i = 1; i < 8; i++) {
+//  console.log("#".repeat(i))
+//}
+
+//while loop
+//exercise 1: keep printing integers in multiples of 3 as long as integers are less than 35 using a while loop
+
