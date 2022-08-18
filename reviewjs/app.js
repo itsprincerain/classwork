195ff76e3b000c0e601d6236bfbb4a919c8995ba
Rainier <rainier.1123@gmail.com>
2022-08-18 14:46:27 -0400

    QnA

---
+++ reviewjs/app.js
@@ -0,0 +1,9 @@
+//               Q+A
+// 1: 3 dways to delcare Variables (var, let, const)
+// 2: selecting the value and typing the new value
+// 3: We simply use the = to give the variable a new value.
+// 4: declare - declaring a variable without assigning a value
+//    assign -  assign() method copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object.
+//    define - The define() function can be used to load the modules (module can be an object, function, class or a code which is executed after loading a module)
+// 5: It allows the designer to focus on main logic without being distracted by programming languages syntax.
+// 6:  I would say 60% of thinking how to code it and the rest of the time should be anwering and coding.
