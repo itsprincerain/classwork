b6c622bb4f9656ee701c63d9a021d21ba933cf5b
Rainier <rainier.1123@gmail.com>
2022-08-18 15:09:03 -0400

    strings

--- reviewjs/app.js
+++ reviewjs/app.js
@@ -7,3 +7,29 @@
 //    define - The define() function can be used to load the modules (module can be an object, function, class or a code which is executed after loading a module)
 // 5: It allows the designer to focus on main logic without being distracted by programming languages syntax.
 // 6:  I would say 60% of thinking how to code it and the rest of the time should be anwering and coding.
+
+
+
+
+
+
+//STRINGS//
+//create a varialble
+let firstVariable; 
+//assign it the value of the string "Hello World"
+firstVariable= "Hello World"
+//change the value of this variable to any number
+firstVariable = 12
+//store the value of firstvariable in a new variable called secondV
+let secondVariable = firstVariable;
+//change the value of secondvariable to any string
+secondVariable = "any"
+
+
+//what is the value of firtsVariable = 12
+console.log(firstVariable)
+ //create a variable YourName
+let yourName = "RAIN RAIN"
+
+console.log(`Hello my name is ${yourName}`)
+
